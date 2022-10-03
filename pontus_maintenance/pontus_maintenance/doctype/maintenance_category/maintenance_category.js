6d76d0467daffbd1a79470029963e1ead734b423
Dharanipath1 <Dharanipath1@users.noreply.github.com>
2022-10-03 11:07:45 +0530

    1st commmit

---
+++ pontus_maintenance/pontus_maintenance/doctype/maintenance_category/maintenance_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Pontus and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Maintenance Category', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ pontus_maintenance/pontus_maintenance/doctype/maintenance_category/maintenance_category.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Pontus and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class MaintenanceCategory(Document):
+	pass
